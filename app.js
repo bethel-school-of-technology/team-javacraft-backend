b41fd75c7f44bb50e54ef6a6d94765885016f8c2
Tanner <tannerjohncummings@gmail.com>
2021-03-14 15:05:07 -0700

    Added Cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const app = express();
+const cors = require("cors");
 
 // MONGO CONNECTION
 const connectionString = "mongodb+srv://dbuser:Password12@bb-menu-items.kuzfo.mongodb.net/menuItems?retryWrites=true&w=majority";
@@ -25,6 +26,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 
 app.use('/items', itemsRoute);
